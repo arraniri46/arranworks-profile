e371dffadeaa83acf48c2188f6fbe983175a3ab0
arraniri46 <garda.arraniri@gmail.com>
2021-10-24 00:44:49 +0700

    atlas mongoDB integrations

--- utils/connect-db.js
+++ utils/connect-db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 // mongoose.connect('mongodb://127.0.0.1:27017/portofolio');
 
-mongoose.connect('mongodb://127.0.0.1:27017/portofolio', (err) => {
+const MONGODB_URI = 'mongodb+srv://admin:stranded2@cluster0.sjby8.mongodb.net/portofolio?retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URI, (err) => {
     if (err){
         throw err;
     } else{
